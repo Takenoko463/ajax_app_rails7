f2ea3f4cfd027540bc2b865f4c14eae61fbc3213
Takenoko463 <83239026+Takenoko463@users.noreply.github.com>
2024-04-03 15:47:51 +0900

    元に戻す

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -5,7 +5,4 @@ set -o errexit
 bundle install
 bundle exec rake assets:precompile
 bundle exec rake assets:clean
-# bundle exec rake db:migrate # データベースを作成?
-
-DISABLE_DATABASE_ENVIRONMENT_CHECK=1 # 本番環境のデーターベースを削除可能に
-bundle exec rake db:migrate:reset # データーベースを削除
+bundle exec rake db:migrate
