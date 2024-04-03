130a45a6e1120f6471919055ead4bfd5942d200e
Takenoko463 <83239026+Takenoko463@users.noreply.github.com>
2024-04-03 15:40:13 +0900

    データーベース削除を設定

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -5,4 +5,7 @@ set -o errexit
 bundle install
 bundle exec rake assets:precompile
 bundle exec rake assets:clean
-bundle exec rake db:migrate
+# bundle exec rake db:migrate # データベースを作成?
+
+DISABLE_DATABASE_ENVIRONMENT_CHECK=1 # 本番環境のデーターベースを削除可能に
+bundle exec rake db:migrate:reset # データーベースを削除
