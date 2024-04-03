679bd0d484f03ba59e4405c0877c57f6289fff3e
Takenoko463 <83239026+Takenoko463@users.noreply.github.com>
2024-04-03 16:13:29 +0900

    テーブル削除設定を除去

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -5,5 +5,5 @@ set -o errexit
 bundle install
 bundle exec rake assets:precompile
 bundle exec rake assets:clean
-# bundle exec rake db:migrate
-DISABLE_DATABASE_ENVIRONMENT_CHECK=1 bundle exec rake db:migrate:reset
+bundle exec rake db:migrate #テーブル再度生成
+# DISABLE_DATABASE_ENVIRONMENT_CHECK=1 bundle exec rake db:migrate:reset #データベース削除 定数の代入は削除のオプション
